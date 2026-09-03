b62b07afcaf7b080e7c06f7f834c4a82b7f92de7
Sylvier01 <106278340+Sylvier01@users.noreply.github.com>
2026-09-03 15:14:28 +0300

    Refactor script.js to include menu icon functionality

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const backToTopBtn = document.querySelector(".back-to-top-btn")
 const navLinks = document.querySelectorAll("nav a")
+const menuIcon = document.querySelector(".menu-icon")
+const navList = document.querySelector(".nav-list")
+const width = window.innerWidth 
+const copyrightYear = document.querySelector(".copyright-year")
+
+/**
+ * Menu icon & Navbar
+ */
+
+// Hide/show menu icon
+if(width < 768){
+    menuIcon.style.display = "block"
+}
+else{
+    menuIcon.style.display = "none"
+}
+
+// Menu icon click events
+menuIcon.addEventListener("click", () => {
+    if(navList.style.display === "block"){
+        navList.style.display = "none"
+    } else{
+        navList.style.display = "block"
+    }
+})
 
 // Active NavLink
 navLinks.forEach(function (link){
@@ -9,10 +34,8 @@ navLinks.forEach(function (link){
             l.classList.remove("active")
         })
 
-        this.classList.add("active")
-        
-    })
-    
+        this.classList.add("active")        
+    })    
 })
 
 // Back to top Btn
@@ -24,3 +47,6 @@ window.addEventListener("scroll", function (){
         backToTopBtn.classList.remove("show")
     }
 })
+
+//Copyright year
+ copyrightYear.textContent = new Date().getFullYear()
